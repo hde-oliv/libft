9bb569155e8adbebe7b945414769a89268205b9d
hde-oliv <henriquerocha@tutanota.com>
2021-06-14 16:33:04 -0300

    Add btree prototypes and struct

--- includes/libft.h
+++ includes/libft.h
@@ -22,6 +22,13 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+typedef struct s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}	t_btree;
+
 /* libc */
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
@@ -79,5 +86,17 @@ int			*ft_range(int min, int max);
 int			ft_abs(int n);
 long long	ft_atoll(const char *str);
 
+/* btree functions */
+t_btree		*btree_create_node(void *item);
+void		btree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void		btree_apply_infix(t_btree *root, void (*applyf)(void *));
+void		btree_apply_suffix(t_btree *root, void (*applyf)(void *));
+void		btree_insert_data(t_btree **root, void *item, \
+int (*cmpf)(void *, void *));
+void		*btree_search_item(t_btree *root, void *data_ref, \
+int *(*cmpf)(void *, void *));
+int			btree_level_count(t_btree *root);
+void		btree_apply_by_level(t_btree *root, \
+void (*applyf)(void *item, int current_level, int is_first_elem));
 
 #endif
