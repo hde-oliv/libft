2fcfa7644629d4b66983a281f30b52cfa506bacb
Henrique Rocha <henriquerocha@tutanota.com>
2021-09-04 15:48:38 -0400

    chore: remove btree struct

--- includes/libft.h
+++ includes/libft.h
@@ -18,16 +18,9 @@
 
 typedef struct s_list
 {
-	void		*content;
+	void			*content;
 	struct s_list	*next;
 }	t_list;
-
-typedef struct s_btree
-{
-	struct s_btree	*left;
-	struct s_btree	*right;
-	void		*item;
-}	t_btree;
 
 /* libc */
 /**
